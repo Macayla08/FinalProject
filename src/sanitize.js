7b886a921f49646f622b4b61d4860b89c2a99490
Macayla08 <maclark2004mc@gmail.com>
2024-12-16 14:43:38 -0500

    Update sanitize.js

--- src/sanitize.js
+++ src/sanitize.js
@@ -4,11 +4,7 @@ const { JSDOM } = require('jsdom');
 const window = new JSDOM('').window;
 const DOMPurify = createDOMPurify(window);
 
-/**
- * Sanitize input to prevent XSS or malicious code.
- * @param {string} dirtyHtml - 
- * @returns {string} 
- */
+
 function sanitizeInput(dirtyHtml) {
     return DOMPurify.sanitize(dirtyHtml);
 }
